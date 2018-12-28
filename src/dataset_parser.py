4ea340d7ea912050bde73b718c65b65c16fd69de
VirajSalaka <viraj.14@cse.mrt.ac.lk>
2018-12-29 00:34:14 +0530

    parse flat with column drop function is added

--- src/dataset_parser.py
+++ src/dataset_parser.py
@@ -26,3 +26,38 @@ def parseFlat():
     x_test  = np.array(x_test)
     
     return x_train, y_train, x_test
+
+#add the indices in an array ex. [1,2,3]
+def parseFlatWith(array):
+    filepath = '../data/'
+    x_train = pd.read_csv(filepath + 'train/x_train.csv')
+    y_train = pd.read_csv(filepath + 'train/y_train.csv')
+    #useless , but added
+    x_test = pd.read_csv(filepath + 'test/x_test.csv')
+    
+    columnArray = ['GeneId', 'H3K4me3','H3K4me1','H3K36me3', 'H3K9me3','H3K27me3']
+    
+    for i in array:
+        columnArray.remove(columnArray[i])
+        
+    x_train = x_train.drop(columnArray,axis=1)
+    x_test = x_test.drop(columnArray,axis=1)
+    y_train = y_train.drop("GeneId",axis=1)
+    
+    no_train_genes = x_train.shape[0] / 100
+    no_test_genes  = x_test.shape[0] / 100
+    
+    x_train = np.split(x_train, no_train_genes)
+    x_test = np.split(x_test, no_test_genes)
+    
+    x_train = [gene.values.ravel() for gene in x_train]
+    x_test = [gene.values.ravel() for gene in x_test]
+    
+    x_train = np.array(x_train)
+    y_train = np.array(y_train.values.ravel())
+    x_test  = np.array(x_test)
+    
+    return x_train, y_train, x_test
+        
+    
+    
